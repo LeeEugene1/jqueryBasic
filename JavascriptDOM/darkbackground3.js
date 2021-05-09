1bb3c566cfe57c7679465a59120062c9a3201ca2
eugene <uwm1004@gmail.com>
2021-05-09 19:26:36 +0900

    attr

--- JavascriptDOM/darkbackground3.js
+++ JavascriptDOM/darkbackground3.js
@@ -1,4 +1,6 @@
 var darkbtn = document.querySelector('#darkbtn')
 darkbtn.addEventListener('click', () => {
     document.body.classList.toggle('dark')
+    // 클래스이름자체를 바꾸고싶으면
+    darkbtn.className = '#darkbtn2'
 });
